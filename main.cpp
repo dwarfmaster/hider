0e20865e5f063c4f17f642611959f7355e23ae46
lucas8 <luc.linux@mailoo.org>
2013-01-25 17:30:45 +0100

    Avancement du main.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <SDL.h>
 #include <iostream>
+#include "options.hpp"
+#include "hider.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -8,7 +10,38 @@ int main(int argc, char *argv[])
 		std::cerr << "Erreur au chargement de la SDL : " << SDL_GetError() << std::endl;
 		return 1;
 	}
+	int end = 0;
+
+	try {
+		Options opts(argc, argv);
+		if( opts.getAct() == Options::HIDE )
+		{
+			Hider hider( opts.getTextPath(), opts.getImgPath() );
+			hider.process();
+		}
+		else
+		{
+			std::cerr << "Cette options n'a pas encore été implémentée." << std::endl;
+			end = 1;
+		}
+	}
+	catch(const std::string& str)
+	{
+		std::cerr << "Une erreur est survenue :\n\t\"" << str << "\"." << std::endl;
+		end = 1;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Une erreur standart est survenue :\n\t\"" << e.what() << "\"." << std::endl;
+		end = 1;
+	}
+	catch(...)
+	{
+		std::cerr << "Une erreur inconnue est survenue." << std::endl;
+		end = 1;
+	}
 
 	SDL_Quit();
+	return end;
 }
 
