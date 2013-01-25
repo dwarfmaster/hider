c91a1a8e0b963af3d035e5768640d5e3242db698
lucas8 <luc.linux@mailoo.org>
2013-01-25 17:54:52 +0100

    Avancement de UnHider::process.

--- unhider.cpp
+++ unhider.cpp
@@ -1,12 +1,49 @@
 
 #include "unhider.hpp"
+#include <iostream>
 
 UnHider::UnHider(boost::filesystem::path dest, boost::filesystem::path img)
-	: m_dest(dest), m_pict(img)
+	: m_pict(img), m_dest(dest)
 {}
 
 bool UnHider::process()
 {
+	unsigned long pos = 0;
+	bool end = false;
+	std::string str("");
+
+	for(Uint32 x = 0; x < m_pict.width(); ++x)
+	{
+		for(Uint32 y = 0; y < m_pict.height(); ++y)
+		{
+			for(int i = 0; i < 3; ++i)
+			{
+				int dec = pos % 4;
+				dec *= 2;
+
+				char c = m_pict.getPart(x, y, i);
+				c <<= dec;
+				str.back() |= c;
+
+				++pos;
+
+				size_t pos = str.find("__EOF__");
+				if( pos != std::string::npos )
+				{
+					i = 4;
+					y = m_pict.height();
+					x = m_pict.width();
+					end = true;
+
+					str = str.substr(0, pos);
+				}
+			}
+		}
+	}
+
+	// TODO enregistrer dans fichier
+	std::cout << str << std::endl;
+	return end;
 }
 
 
