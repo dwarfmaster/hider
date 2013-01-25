0d2f6772e026ccfd7243f6a39a5fed6cfa729a1d
lucas8 <luc.linux@mailoo.org>
2013-01-25 17:23:53 +0100

    Fin de la classe Hider.

--- hider.cpp
+++ hider.cpp
@@ -13,10 +13,44 @@ Hider::Hider(boost::filesystem::path src, boost::filesystem::path img)
 	std::string line;
 	while( std::getline(ifs, line) )
 		m_src += line;
+
+	m_src += "__EOF__";
 }
 
 bool Hider::process()
 {
+	unsigned long pos = 0;
+	bool end = false;
+	for(Uint32 x = 0; x < m_pict.width(); ++x)
+	{
+		for(Uint32 y = 0; y < m_pict.height(); ++y)
+		{
+			for(int i = 0; i < 3; ++i)
+			{
+				size_t cpos = pos / 4; // Position du caractère
+				if( cpos >= m_src.size() ) // On sort de la boucle en cas de fin
+				{
+					i = 4;
+					y = m_pict.height();
+					x = m_pict.width();
+					end = true;
+				}
+				else
+				{
+					int dec = pos % 4; // Le bloc de bit
+					dec *= 2;
+
+					char c = m_src[cpos];
+					c &= (3 << dec); // 0b11 << dec
+					c >>= dec;
+
+					m_pict.setPart(x, y, i, c);
+					++pos;
+				}
+			}
+		}
+	}
+	return end;
 }
 
 
